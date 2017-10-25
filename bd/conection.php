fecaa7c9414a93893d50b8036c9ea1029b60af3a
Sarah Yasmin <syasmin2088@gmail.com>
2017-10-25 15:50:00 -0200

    classe de conexão criada

---
+++ bd/conection.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of conection
+ *
+ * @author syasmin20
+ */
+
+class conection {
+ 
+    public static $instance;
+   
+      private function __construct() {
+          //
+      }
+   
+      public static function getInstance() {
+          if (!isset(self::$instance)) {
+              self::$instance = new PDO('mysql:host=localhost;dbname=CatalogoFilme', 'root', 'bd123', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+              self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+          }
+   
+          return self::$instance;
+      }
+   
+
+}
+  
+   
